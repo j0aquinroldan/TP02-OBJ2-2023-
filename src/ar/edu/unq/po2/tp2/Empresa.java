58530efb5f112e87f91e7c314304ae34d245d6db
Joaquin <jroldanarin@gmail.com>
2023-03-23 13:30:09 -0300

    se agregaron los mensajes de los montos de sueldo bruto, neto y retenciones

--- src/ar/edu/unq/po2/tp2/Empresa.java
+++ src/ar/edu/unq/po2/tp2/Empresa.java
@@ -7,6 +7,60 @@ public class Empresa {
 	private String nombre;
 	private String cuit;
 	private List<Empleado> empleados;
+	
+	public Empresa(String nombre, String cuit, List<Empleado> empleados) {
+		this.setNombre(nombre);
+		this.setCuit(cuit);
+		this.setEmpleados(empleados);
+	}
 
+	public Empresa(String nombre, String cuit) {
+		this.setNombre(nombre);
+		this.setCuit(cuit);
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCuit() {
+		return cuit;
+	}
+
+	public void setCuit(String cuit) {
+		this.cuit = cuit;
+	}
+
+	public List<Empleado> getEmpleados() {
+		return empleados;
+	}
+
+	public void setEmpleados(List<Empleado> empleados) {
+		this.empleados = empleados;
+	}
+
+	public void contratar(Empleado e) {
+		this.getEmpleados().add(e);
+	}
+	
+	public void despedir(Empleado e) {
+		this.getEmpleados().remove(e);
+	}
+
+	public double getMontoTotalSueldosNetos() {
+		return this.getEmpleados().stream().mapToDouble(e -> e.getSueldoNeto()).sum();
+	}
+	
+	public double getMontoTotalSueldosBrutos() {
+		return this.getEmpleados().stream().mapToDouble(e -> e.getSueldoBruto()).sum();
+	}
+	
+	public double getMontoTotalRetenciones() {
+		return this.getEmpleados().stream().mapToDouble(e -> e.getRetenciones()).sum();
+	}
 	
 }
