79bc2b644fbd5903f8c37fc73b69014891e4a418
Joaquin <jroldanarin@gmail.com>
2023-03-24 17:55:56 -0300

    creada clase recibohaberes

---
+++ src/ar/edu/unq/po2/tp2/ReciboHaberes.java
@@ -0,0 +1,46 @@
+package ar.edu.unq.po2.tp2;
+
+import java.time.LocalDate;
+
+public class ReciboHaberes {
+
+	private String nombre;
+	private String dirección;
+	private LocalDate fechaEmisión;
+	private double sueldoBruto;
+	private double SueldoNeto;
+
+	public ReciboHaberes(String nombre, String dirección, LocalDate fechaEmisión, double sueldoBruto,
+			double sueldoNeto) {
+		this.setNombre(nombre);
+		this.setDirección(dirección);
+		this.setFechaEmisión(fechaEmisión);
+		this.setSueldoBruto(sueldoBruto);
+		this.setSueldoNeto(sueldoNeto);
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void setDirección(String dirección) {
+		this.dirección = dirección;
+	}
+
+	public void setFechaEmisión(LocalDate fechaEmisión) {
+		this.fechaEmisión = fechaEmisión;
+	}
+
+	public void setSueldoBruto(double sueldoBruto) {
+		this.sueldoBruto = sueldoBruto;
+	}
+
+	public void setSueldoNeto(double sueldoNeto) {
+		SueldoNeto = sueldoNeto;
+	}
+	
+	public LocalDate getFechaEmisión() {
+		return fechaEmisión;
+	}
+
+}
